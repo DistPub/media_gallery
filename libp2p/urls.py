348a8578ac60d4ece6830b67634b12cbae8648e7
Smite Chow <xiaopengyou2no1@gmail.com>
2020-12-04 11:41:59 +0800

    use custom ice servers

--- libp2p/urls.py
+++ libp2p/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from libp2p import views
+
 urlpatterns = [
-    path('', TemplateView.as_view(
-        template_name="libp2p/index.html"
-    ), name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('sw.js', TemplateView.as_view(
         template_name="libp2p/sw.js", content_type='application/javascript'
     ), name='sw.js'),
