80c24d8bb2cc52393fd40ef51413872536c4059e
Smite Chow <xiaopengyou2no1@gmail.com>
2020-12-09 19:07:02 +0800

    remove online bundle link

---
+++ libp2p/static/libp2p/dep.js
@@ -0,0 +1,4 @@
+window.libp2pNoise = require('libp2p-noise');
+window.libp2pMplex = require('libp2p-mplex');
+window.itPipe = require('it-pipe');
+window.datastoreLevel = require('datastore-level');
