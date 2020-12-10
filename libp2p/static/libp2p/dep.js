7f541ea9aa1e4f615d6695a2768aadb05005697d
Smite Chow <xiaopengyou2no1@gmail.com>
2020-12-10 11:16:58 +0800

    upgrade libp2p and libp2p-webrtc-star

--- libp2p/static/libp2p/dep.js
+++ libp2p/static/libp2p/dep.js
@@ -2,3 +2,5 @@ window.libp2pNoise = require('libp2p-noise');
 window.libp2pMplex = require('libp2p-mplex');
 window.itPipe = require('it-pipe');
 window.datastoreLevel = require('datastore-level');
+window.libp2p = require('libp2p');
+window.libp2pWebrtcStar = require('libp2p-webrtc-star');
