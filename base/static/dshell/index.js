97ccbddeb1d7c16f292abc621b7b716fe0a5ba83
Smite Chow <xiaopengyou2no1@gmail.com>
2021-01-05 13:22:41 +0800

    use cdn dshell

--- base/static/dshell/index.js
+++
@@ -1,4 +0,0 @@
-import Shell from './shell.js'
-import UserNode from './user-node.js'
-import Soul from './soul.js'
-export { Shell, UserNode, Soul }
--- base/static/dshell/utils.js
+++
@@ -1,6 +0,0 @@
-export function log(text) {
-  console.log(text)
-}
-export const AsyncFunction = (async () => {}).constructor
-export const AsyncGeneratorFunction = (async function* () {}).constructor
-export const GeneratorFunction = (function* () {}).constructor
