f014cedfe3b3da825864adae109556895969ec10
Smite Chow <xiaopengyou2no1@gmail.com>
2021-01-21 16:36:37 +0800

    improve

--- avideo_chat/static/avideo_chat/js/index.jsx
+++ avideo_chat/static/avideo_chat/js/index.jsx
@@ -2,7 +2,20 @@ const root = document.getElementById('root')
 
 function Welcome(props) {
   console.log(props)
-  return <h1>Hello, {props.name.toLocaleTimeString()}</h1>
+  const [time, setTime] = React.useState(new Date())
+
+  React.useEffect(() => {
+    const timer = setInterval(() => setTime(oldTime => {
+      const newTime = new Date()
+      console.log(`from old: ${oldTime} to ${newTime}`)
+      return newTime
+    }), 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  return <h1>Hello, {time.toLocaleTimeString()}</h1>
 }
 
-setInterval(()=>ReactDOM.render(<Welcome name={new Date()}/>, root), 1000)
+ReactDOM.render(<Welcome/>, root)
