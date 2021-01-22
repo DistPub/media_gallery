ddf7a1682a4c19826cf3443644555380213e269c
Smite Chow <xiaopengyou2no1@gmail.com>
2021-01-22 11:17:42 +0800

    improve

--- avideo_chat/static/avideo_chat/js/index.jsx
+++ avideo_chat/static/avideo_chat/js/index.jsx
@@ -45,7 +45,10 @@ function Input(props) {
 
 function Select(props) {
   const [value, setValue] = React.useState(props.value)
-  return <select value={value} onChange={(event)=>setValue(event.target.value)}>
+  return <select name={props.name} value={value} onChange={(event)=>{
+    console.log(`change ${event.target.name} value to ${event.target.value}`)
+    setValue(event.target.value)
+  }}>
     <option value={'hi0'}>hi</option>
     <option value={'hi1'}>hi</option>
     <option value={'hi2'}>hi</option>
@@ -54,11 +57,7 @@ function Select(props) {
 
 function Root() {
   const [show, setShow] = React.useState({1: true, 2: false, 3: false})
-  window.toggle = (n) => setShow(v=>{
-    const update = {}
-    update[n] = !v[n]
-    return {...v, ...update}
-  })
+  window.toggle = (n) => setShow(v=>{return {...v, [n]: !v[n]}})
   console.log(`render root ${JSON.stringify(show)}`)
   return <div>
     {show[1] && <Welcome id={1}/>}
@@ -66,7 +65,7 @@ function Root() {
     {show[3] && <Welcome id={3}/>}
     <List list={'abcdefg'}/>
     <Input value={'hello smite'}/>
-    <Select value={'hi3'}/>
+    <Select value={'hi3'} name={'selectWidget'}/>
   </div>
 }
 
