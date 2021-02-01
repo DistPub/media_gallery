a106d3a207ec41151c56be7f9180ee5bae0a31f2
Smite Chow <xiaopengyou2no1@gmail.com>
2021-02-01 13:30:23 +0800

    check dshell status

--- zgvg/static/zgvg/js/index.jsx
+++ zgvg/static/zgvg/js/index.jsx
@@ -1,7 +1,46 @@
+function installAction() {
+  console.log(`installed`)
+}
+
+function exportActiveOrder() {
+  console.log(`exported`)
+}
+
 function Root(props) {
-  return <button className="ui green button">
+  const [depStatus, setDepStatus] = React.useState(false)
+  const notReadyUI = <div className="ui icon message">
+    <i className="notched circle loading icon"></i>
+    <div className="content">
+      <div className="header">
+        请稍等
+      </div>
+      <p>执行单导出功能加载中...</p>
+    </div>
+  </div>
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      try {
+        if(dshell.userNode.node.isStarted()) {
+          clearInterval(timer)
+          setDepStatus(true)
+        }
+      } catch {
+        // dep not resolved
+      }
+    }, 500)
+  }, [])
+
+  if (!depStatus) {
+    return notReadyUI
+  }
+
+  installAction()
+
+  return <button className="ui green button" onClick={exportActiveOrder}>
     <i className="eye icon"></i>
     执行单导出
   </button>
 }
+
 ReactDOM.render(<Root/>, root)
