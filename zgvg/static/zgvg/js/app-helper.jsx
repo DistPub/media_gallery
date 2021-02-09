0a765d21f08e4ae8de8fab94af1345b8b7e143ab
Smite Chow <xiaopengyou2no1@gmail.com>
2021-02-09 11:17:41 +0800

    specified browser to Chrome

--- zgvg/static/zgvg/js/app-helper.jsx
+++ zgvg/static/zgvg/js/app-helper.jsx
@@ -1,6 +1,6 @@
 export default function AppHelper(props) {
   return <div className="ui message">
-    <div className="header">[应用故障]请尝试升级浏览器或稍后重试</div>
+    <div className="header">[应用故障]请升级Chrome或稍后重试</div>
     <p>错误信息</p>
     <code>{props.error.stack}</code>
   </div>
