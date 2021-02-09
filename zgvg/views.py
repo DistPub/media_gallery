d6f505c9710a28bc1a1c694c2e976f0af0b31dcd
Smite Chow <xiaopengyou2no1@gmail.com>
2021-02-09 11:12:56 +0800

    custom lang

--- zgvg/views.py
+++ zgvg/views.py
@@ -7,4 +7,4 @@ class Index(TemplateView):
     template_name = 'zgvg/index.html'
 
     def get_context_data(self, **kwargs):
-        return {'iceServers': ICE_SERVERS_LIST_JSON_ENCODE}
+        return {'iceServers': ICE_SERVERS_LIST_JSON_ENCODE, 'lang': 'zh_CN'}
