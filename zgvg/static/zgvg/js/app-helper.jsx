150b0793dffa6d9af86a8ef3a48b16100a9b1e1e
Smite Chow <xiaopengyou2no1@gmail.com>
2021-03-01 10:02:26 +0800

    improve helper component

--- zgvg/static/zgvg/js/app-helper.jsx
+++ zgvg/static/zgvg/js/app-helper.jsx
@@ -1,6 +1,7 @@
 export default function AppHelper(props) {
   return <div className="ui message">
     <div className="header">[应用故障]请升级Chrome或稍后重试</div>
+    {props.error.message.includes('Failed to fetch dynamically imported module') && <p>请确保chrome://flags/#enable-experimental-web-platform-features开启</p>}
     <p>错误信息</p>
     <code>{props.error.stack}</code>
   </div>
