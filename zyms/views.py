5607370282dba83817d4c15f9ee4613cb0f08687
Smite Chow <xiaopengyou2no1@gmail.com>
2021-05-25 11:08:06 +0800

    flag to remove body class

--- zyms/views.py
+++ zyms/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = 'zyms/index.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['remove_body_class'] = True
+        return data
