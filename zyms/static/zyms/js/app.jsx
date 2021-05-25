ee1d0dddabdbea6f73140a6502199b74b67e3027
Smite Chow <xiaopengyou2no1@gmail.com>
2021-05-25 10:53:00 +0800

    refactoring context

--- zyms/static/zyms/js/app.jsx
+++ zyms/static/zyms/js/app.jsx
@@ -1,44 +1,24 @@
-import dshell from 'https://cdn.jsdelivr.net/npm/dshell@1.4.0/dshell.js'
-import {ShellContext} from "./context.js"
-
-var db = new PouchDB('todos');
-
-let username = sessionStorage.getItem('username');
-
-if (!username) {
-    username = prompt("请输入用户名", "<username>");
-    sessionStorage.setItem('username', username);
-}
-
-let password = sessionStorage.getItem('password');
-
-if (!password) {
-    password = prompt("请输入密码", "<password>");
-    sessionStorage.setItem('password', password);
-}
-
-var remoteCouch = `https://${username}:${password}@relax.smitechow.com/todos`;
-var opts = {live: true};
-db.replicate.to(remoteCouch, opts, console.log);
-db.replicate.from(remoteCouch, opts, console.log);
-
-function addTodo(text) {
-  var todo = {
-    _id: new Date().toISOString(),
-    title: text,
-    completed: false
-  };
-  db.put(todo, function callback(err, result) {
-    if (!err) {
-      console.log('Successfully posted a todo!');
-    }
-  });
-}
+import {ShellContext, PouchDBContext} from "./context.js"
 
 export default function App(props) {
-  const [shell, setShell] = React.useState(null);
+  const db = React.useContext(PouchDBContext);
+  const shell = React.useContext(ShellContext);
+
   const [todos, setTodos] = React.useState([]);
   const [title, setTitle] = React.useState('');
+
+  function addTodo(text) {
+    var todo = {
+      _id: new Date().toISOString(),
+      title: text,
+      completed: false
+    };
+    db.put(todo, function callback(err, result) {
+      if (!err) {
+        console.log('Successfully posted a todo!');
+      }
+    });
+  }
 
   function showTodos() {
     db.allDocs({include_docs: true, descending: true}, function(err, doc) {
@@ -46,20 +26,17 @@ export default function App(props) {
     });
   }
 
-  db.changes({
-    since: 'now',
-    live: true
-  }).on('change', showTodos);
-
-  React.useEffect(async () => {
-    await dshell.init()
-    setShell(dshell);
-
+  React.useEffect(() => {
     showTodos();
+
+    db.changes({
+      since: 'now',
+      live: true
+    }).on('change', showTodos);
   }, [])
 
-  return <ShellContext.Provider value={shell}>
-    <input onChange={(event)=>setTitle(event.target.value)}></input>
+  return <>
+    <input onChange={(event)=>setTitle(event.target.value)} />
     <br/>
     <button onClick={() => addTodo(title)}>add</button>
     <div className="ui divider"/>
@@ -71,5 +48,5 @@ export default function App(props) {
           }}>edit</button>
         <button onClick={() => db.remove(item.doc)}>delete</button></li>)}
     </ul>
-  </ShellContext.Provider>
+  </>
 }
--- zyms/static/zyms/js/context.js
+++ zyms/static/zyms/js/context.js
@@ -1,2 +1,30 @@
-export const ShellContext = React.createContext(null)
-export const ModalContainer = React.createContext(document.querySelector('#modal'))
+import dshell from 'https://cdn.jsdelivr.net/npm/dshell@1.4.0/dshell.js';
+
+await dshell.init();
+export const ShellContext = React.createContext(dshell);
+export const ModalContainer = React.createContext(document.querySelector('#modal'));
+
+function initDBContext(name) {
+  let db = new PouchDB(name);
+  let username = sessionStorage.getItem('username');
+
+  if (!username) {
+    username = prompt("请输入用户名", "<username>");
+    sessionStorage.setItem('username', username);
+  }
+
+  let password = sessionStorage.getItem('password');
+
+  if (!password) {
+    password = prompt("请输入密码", "<password>");
+    sessionStorage.setItem('password', password);
+  }
+
+  let remoteCouch = `https://${username}:${password}@relax.smitechow.com/todos`;
+  let opts = {live: true};
+  db.replicate.to(remoteCouch, opts, console.error);
+  db.replicate.from(remoteCouch, opts, console.error);
+  return React.createContext(db);
+}
+
+export const PouchDBContext = initDBContext('todos')
