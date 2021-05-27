d2581fc7dbbf67293d44e326c84c9e368774139d
Smite Chow <xiaopengyou2no1@gmail.com>
2021-05-27 11:20:34 +0800

    add batch component

--- zyms/static/zyms/js/app.jsx
+++ zyms/static/zyms/js/app.jsx
@@ -1,6 +1,7 @@
 import styles from '../css/app.cssm' assert { type: "css" }
 import SideBar from './side-bar.jsx';
 import ZYTable from './zy-table.jsx';
+import BatchOperateZY from './batch-operate-zy.jsx';
 
 const tabs = {
   table: '资源管理',
@@ -18,6 +19,7 @@ export default function App(props) {
     <SideBar tabs={tabs} tabName={tab} showTab={(name) => setTab(name)}/>
     <div className={`${styles['tab-container']}`}>
       {tab === 'table' && <ZYTable/>}
+      {tab === 'batch' && <BatchOperateZY/>}
     </div>
   </div>
 }
