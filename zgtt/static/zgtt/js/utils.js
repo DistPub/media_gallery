7a943d124b8814e2da20e36b99187ee073339ec8
Smite Chow <xiaopengyou2no1@gmail.com>
2021-08-12 18:26:47 +0800

    improve message

--- zgtt/static/zgtt/js/utils.js
+++ zgtt/static/zgtt/js/utils.js
@@ -1,14 +1,26 @@
-import {ChromeExtensionID} from 'settings';
+import uuidv4 from 'https://cdn.jsdelivr.net/npm/uuid@8.3.2/dist/esm-browser/v4.js';
 
 export function absURL(relative, base) {
   const url = new URL(relative, base)
   return url.href
 }
 
+const messageCallback = {};
+window.addEventListener('message', event=>{
+  if (event.source !== window) {
+    return;
+  }
+
+  if (event.data.source === 'edge-lover-bridge') {
+    messageCallback[event.data.id](event.data.payload);
+    delete messageCallback[event.data.id];
+  }
+});
+
 export function requestExtension(message) {
-  return new Promise(resolve => {
-    chrome.runtime.sendMessage(ChromeExtensionID, message, resolve)}
-  )
+  let id = uuidv4();
+  window.postMessage({destination: 'edge-lover-bridge', payload: message, id});
+  return new Promise(resolve => messageCallback[id] = resolve);
 }
 
 export function getCode(func) {
