f5a75c7c300a4b7571e8a9aaa6a79cc204c2381c
Smite Chow <xiaopengyou2no1@gmail.com>
2021-09-17 14:58:26 +0800

    promise all

--- zgtt/static/zgtt/js/export-hot-account-button.jsx
+++ zgtt/static/zgtt/js/export-hot-account-button.jsx
@@ -7,7 +7,9 @@ function makeFlow(shell, hotID, namespace) {
     .HotAccountDetail.using(null) // info
     .Collect // => [row, ...]
     .buildExcel(['data',
-      ["账号名", "粉丝量", "账号ID", "星图20秒价格", "星图60秒价格", "20S cpm", "60S cpm", "简介", "完播率", "星图指数", "MCN",
+      ["账号名", "粉丝量", "账号ID", "星图20秒价格", "星图60秒价格", "20S cpm", "60S cpm", "简介", "星图视频完播率",
+        "星图视频互动率","星图视频latest15播放量","星图视频latest15点赞量",
+        "星图指数", "MCN",
       "标签", "地区", "观众男性占比", "观众女性占比", "观众地域占比（top10）",
         "粉丝男性占比", "粉丝女性占比", "粉丝地域占比（top10）","粉丝活跃度"]
     ])
@@ -46,11 +48,11 @@ async function* HotAccount(di, hotID) {
       let response = await fetch(api, {mode: 'cors', credentials: 'include'})
       response = await response.json()
 
-      setTotal(old => old + response.data.stars.length * 7)
+      setTotal(old => old + response.data.stars.length * 8)
 
       for (let item of response.data.stars) {
         if (cache.includes(item.id)){
-          setTotal(old => old - 7)
+          setTotal(old => old - 8)
           continue;
         } else {
           cache.push(item.id);
@@ -77,11 +79,7 @@ async function HotAccountDetail(di, args) {
   let paramsString = `o_author_id${id}platform_channel1platform_source1service_methodGetAuthorPlatformChannelInfoV2service_nameauthor.AdStarAuthorService`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
-
-  let response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let intro = response.data?.self_intro;
-  setComplete(old => old + 1)
+  let fetch1 = fetch(api, {mode: 'cors', credentials: 'include'});
 
   api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
   api.searchParams.append('o_author_id', id)
@@ -93,13 +91,9 @@ async function HotAccountDetail(di, args) {
   paramsString = `o_author_id${id}platform_channel1platform_source1service_methodAuthorScoreV2service_nameauthor.AdStarAuthorService`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
+  let fetch2 = fetch(api, {mode: 'cors', credentials: 'include'});
 
-  response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let score = response.data.top_score
-  setComplete(old => old + 1)
-
-    api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
+  api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
   api.searchParams.append('o_author_id', id)
   api.searchParams.append('platform_source', 1)
   api.searchParams.append('platform_channel', 1)
@@ -109,14 +103,9 @@ async function HotAccountDetail(di, args) {
   paramsString = `o_author_id${id}platform_channel1platform_source1service_methodGetAuthorMarketingInfoservice_nameauthor.AdStarAuthorService`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
+  let fetch3 = fetch(api, {mode: 'cors', credentials: 'include'});
 
-  response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let price20 = response.data.price_info[0]?.price
-  let price60 = response.data.price_info[1]?.price
-  setComplete(old => old + 1)
-
-    api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
+  api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
   api.searchParams.append('o_author_id', id)
   api.searchParams.append('platform_source', 1)
   api.searchParams.append('platform_channel', 1)
@@ -127,15 +116,7 @@ async function HotAccountDetail(di, args) {
   paramsString = `o_author_id${id}platform_channel1platform_source1recommendrecommendservice_methodGetAuthorBaseInfoservice_nameauthor.AdStarAuthorService`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
-
-  response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let short_id = response.data.short_id
-    let follower = response.data.follower
-  let mcn = response.data.mcn_name
-  let tags = Object.keys(response.data.tags_relation).join(', ')
-  let address = `${response.data.province}-${response.data.city}`
-  setComplete(old => old + 1)
+  let fetch4 = fetch(api, {mode: 'cors', credentials: 'include'})
 
   api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
   api.searchParams.append('o_author_id', id)
@@ -149,12 +130,7 @@ async function HotAccountDetail(di, args) {
   paramsString = `o_author_id${id}platform_channel1platform_source1range2service_methodGetAuthorSpreadInfoservice_namedata.AdStarDataServicetype2`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
-
-  response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let playOverRate = response.data.play_over_rate.value
-  let {cpm_1_20:cpm20, cpm_21_60:cpm60} = response.data.expect_cpm
-  setComplete(old => old + 1)
+  let fetch5 = fetch(api, {mode: 'cors', credentials: 'include'})
 
   api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
   api.searchParams.append('o_author_id', id)
@@ -168,22 +144,7 @@ async function HotAccountDetail(di, args) {
   paramsString = `o_author_id${id}platform_channel1platform_source1service_methodGetAuthorWatchedDistributionservice_namedata.AdStarDataServicesign_strict1type1`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
-
-  response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let sexDistributions = response.data.distributions[1]
-  let [men, women] = sexDistributions.distribution_list
-  men = parseInt(men.distribution_value)
-  women = parseInt(women.distribution_value)
-
-  let locationDistributions = response.data.distributions[3]
-  let locationAll = locationDistributions.distribution_list.map(item=>parseInt(item.distribution_value)).reduce((a,b)=>{
-    return a+b;
-  }, 0)
-  let locationTop10 = locationDistributions.distribution_list.slice(0, 10).map(item=>{
-    return `${item.distribution_key}: ${(item.distribution_value/locationAll*100).toFixed(2)}%`
-  }).reduce((a,b)=>[a,b].join(', '))
-  setComplete(old => old + 1)
+  let fetch6 = fetch(api, {mode: 'cors', credentials: 'include'})
 
   api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
   api.searchParams.append('o_author_id', id)
@@ -196,31 +157,87 @@ async function HotAccountDetail(di, args) {
   paramsString = `author_type1o_author_id${id}platform_source1service_methodGetAuthorFansDistributionV2service_namedata.AdStarDataServicesign_strict1`
   paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
   api.searchParams.append('sign', md5(paramsString))
+  let fetch7 = fetch(api, {mode: 'cors', credentials: 'include'})
 
-  response = await fetch(api, {mode: 'cors', credentials: 'include'})
-  response = await response.json()
-  let sexDistributionsFans = response.data.distributions[1]
+  api = new URL(atob("aHR0cHM6Ly93d3cueGluZ3R1LmNuL2gvYXBpL2dhdGV3YXkvaGFuZGxlcl9nZXQv"))
+  api.searchParams.append('o_author_id', id)
+  api.searchParams.append('platform_source', 1)
+  api.searchParams.append('platform_channel', 1)
+  api.searchParams.append('limit', 10)
+  api.searchParams.append('sign_strict', 1)
+  api.searchParams.append('service_name', 'author.AdStarAuthorService')
+  api.searchParams.append('service_method', 'GetAuthorShowItemsV2')
+
+  paramsString = `limit10o_author_id${id}platform_channel1platform_source1service_methodGetAuthorShowItemsV2service_nameauthor.AdStarAuthorServicesign_strict1`
+  paramsString = paramsString + 'e39539b8836fb99e1538974d3ac1fe98'
+  api.searchParams.append('sign', md5(paramsString))
+  let fetch8 = fetch(api, {mode: 'cors', credentials: 'include'})
+
+  let responses = await Promise.all([
+    fetch1, fetch2, fetch3, fetch4, fetch5, fetch6, fetch7, fetch8
+  ])
+  let [response1,response2,response3,response4,response5,response6,response7,response8] = await Promise.all(
+    responses.map(item=>item.json())
+  )
+
+  let intro = response1.data?.self_intro;
+  let score = response2.data.top_score
+  let price20 = response3.data.price_info[0]?.price
+  let price60 = response3.data.price_info[1]?.price
+  let short_id = response4.data.short_id
+  let follower = response4.data.follower
+  let mcn = response4.data.mcn_name
+  let tags = Object.keys(response4.data.tags_relation).join(', ')
+  let address = `${response4.data.province}-${response4.data.city}`
+  let playOverRate = response5.data.play_over_rate.value
+  if(playOverRate!==undefined) {
+    playOverRate = (playOverRate/100).toFixed(1)+'%';
+  }
+  let interactRate = response5.data.interact_rate.value
+  if(interactRate!==undefined) {
+    interactRate = (interactRate/100).toFixed(1)+'%';
+  }
+  let {cpm_1_20:cpm20, cpm_21_60:cpm60} = response5.data.expect_cpm
+  let sexDistributions = response6.data.distributions[1]
+  let [men, women] = sexDistributions.distribution_list
+  men = parseInt(men.distribution_value)
+  women = parseInt(women.distribution_value)
+  let locationDistributions = response6.data.distributions[3]
+  let locationAll = locationDistributions.distribution_list.map(item=>parseInt(item.distribution_value)).reduce((a,b)=>{
+    return a+b;
+  }, 0)
+  let locationTop10 = locationDistributions.distribution_list.slice(0, 10).map(item=>{
+    return `${item.distribution_key}: ${(item.distribution_value/locationAll*100).toFixed(2)}%`
+  }).reduce((a,b)=>[a,b].join(', '))
+  let sexDistributionsFans = response7.data.distributions[1]
   let [menFans, womenFans] = sexDistributionsFans.distribution_list
   menFans = parseInt(menFans.distribution_value)
   womenFans = parseInt(womenFans.distribution_value)
-
-  let locationDistributionsFans = response.data.distributions[3]
+  let locationDistributionsFans = response7.data.distributions[3]
   let locationAllFans = locationDistributionsFans.distribution_list.map(item=>parseInt(item.distribution_value)).reduce((a,b)=>{
     return a+b;
   }, 0)
   let locationTop10Fans = locationDistributionsFans.distribution_list.slice(0, 10).map(item=>{
     return `${item.distribution_key}: ${(item.distribution_value/locationAllFans*100).toFixed(2)}%`
   }).reduce((a,b)=>[a,b].join(', '))
-  let hyd = response.data.distributions[2]
+  let hyd = response7.data.distributions[2]
   let hydAll = hyd.distribution_list.map(item=>parseInt(item.distribution_value)).reduce((a,b)=>{
     return a+b;
   }, 0)
   hyd = hyd.distribution_list.map(item=>{
     return `${item.distribution_key}: ${(item.distribution_value/hydAll*100).toFixed(2)}%`
   }).reduce((a,b)=>[a,b].join(', '))
-  setComplete(old => old + 1)
+  let items = response8.data.latest_star_item_info
+  let min=Math.min(...items.map(item=>item.play));
+  let likeMin=Math.min(...items.map(item=>item.like));
+  let max=Math.max(...items.map(item=>item.play));
+  let likeMax=Math.max(...items.map(item=>item.like));
+  let playDesc = `最新15个视频中最低播放量 ${(min/10000).toFixed(1)}w 最高播放量 ${(max/10000).toFixed(1)}w`
+  let likeDesc = `最新15个视频中最低点赞量 ${(likeMin/10000).toFixed(1)}w 最高点赞量 ${(likeMax/10000).toFixed(1)}w`
 
-  return [...info, follower, short_id, price20, price60, cpm20, cpm60, intro, playOverRate, score, mcn, tags, address,
+  return [...info, follower, short_id, price20, price60, cpm20, cpm60, intro, playOverRate,
+    interactRate,playDesc,likeDesc,
+    score, mcn, tags, address,
     (men/(men+women)*100).toFixed(2)+'%', (women/(men+women)*100).toFixed(2)+'%', locationTop10,
     (menFans/(menFans+womenFans)*100).toFixed(2)+'%', (womenFans/(menFans+womenFans)*100).toFixed(2)+'%',
     locationTop10Fans, hyd]
